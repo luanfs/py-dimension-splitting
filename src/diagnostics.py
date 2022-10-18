76292647e4db7e117ee1bd88292a29e6976c8232
luanfs <luanfonseca93@gmail.com>
2022-10-18 10:56:43 -0300

    added colormap

--- src/diagnostics.py
+++ src/diagnostics.py
@@ -114,4 +114,4 @@ def output_adv(Xc, Yc, simulation, Q, error_linf, error_l1, error_l2, plot, k, t
             Uplot[0:nplot,0:nplot], Vplot[0:nplot,0:nplot]  = velocity_adv_2d(xplot, yplot, t, simulation)
             filename = graphdir+'2d_adv_tc'+str(tc)+'_ic'+str(ic)+'_t'+str(k)+'_N'+str(N)+'_'+simulation.fvmethod+'_mono'+simulation.monot+'.png'
             title = '2D advection - '+icname+' - time='+str(t)+', CFL='+str(CFL)+',\n N='+str(N)+', '+simulation.fvmethod+', mono = '+simulation.monot+ ', Min = '+ qmin +', Max = '+qmax
-            plot_2dfield_graphs([Q[i0:iend,j0:jend]], [Qmin], [Qmax],Xc, Yc, [Uplot], [Vplot], xplot, yplot, filename, title)
+            plot_2dfield_graphs([Q[i0:iend,j0:jend]], [Qmin], [Qmax], ['jet'], Xc, Yc, [Uplot], [Vplot], xplot, yplot, filename, title)
--- src/plot.py
+++ src/plot.py
@@ -13,10 +13,10 @@ from errors import *
 ####################################################################################
 # Plot the 2d graphs given in the list fields
 ####################################################################################
-def plot_2dfield_graphs(scalar_fields, scalar_fieldsmin, scalar_fieldsmax, xplot, yplot, vector_fieldsu, vector_fieldsv, xv, yv, filename, title):
+def plot_2dfield_graphs(scalar_fields, scalar_fieldsmin, scalar_fieldsmax, cmaps, xplot, yplot, vector_fieldsu, vector_fieldsv, xv, yv, filename, title):
     n = len(scalar_fields)
     for k in range(0, n):
-        plt.contourf(xplot, yplot, scalar_fields[k], cmap='jet', levels=np.linspace(scalar_fieldsmin[k],scalar_fieldsmax[k],101))
+        plt.contourf(xplot, yplot, scalar_fields[k], cmap=cmaps[k], levels=np.linspace(scalar_fieldsmin[k],scalar_fieldsmax[k],101))
         plt.colorbar(orientation='vertical', fraction=0.046, pad=0.04)
         rnorm = 1.0
         #rnorm = np.sqrt(vector_fieldsu[k]**2 + vector_fieldsv[k]**2)
