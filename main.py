ec19652f9c05de7864289594d40bf4b147572aa7
luanfs <luanfonseca93@gmail.com>
2022-10-18 13:15:35 -0300

    added sw

--- main.py
+++ main.py
@@ -16,24 +16,35 @@ import configuration as conf
 from miscellaneous           import createDirs
 
 from advection_2d            import adv_2d
-from parameters_2d           import simulation_par_2d
+from parameters_2d           import simulation_adv_par_2d
 from error_adv_2d            import error_analysis_adv2d
 
 # Create directories
 createDirs()
 
-# 2D advection test cases - parameters from par/configuration.par
 # Get parameters
-N, M, dt, Tf, tc, ic, vf, mono = conf.get_test_parameters_2d('configuration.par')
-simulation = simulation_par_2d(N, M, dt, Tf, ic, vf, tc, mono)
+N, M, eq = conf.get_test_parameters_2d('configuration.par')
 
 # Select test case
-if tc == 1:
-    # Advection routine
-    adv_2d(simulation, True)
-elif tc == 2:
-    # Advection error analysis
-    error_analysis_adv2d(simulation)
+if eq == 1:
+    # 2D advection test cases - parameters from par/configuration.par
+    # Get parameters
+    dt, Tf, tc, ic, vf, mono = conf.get_adv_parameters_2d('advection.par')
+    simulation = simulation_adv_par_2d(N, M, dt, Tf, ic, vf, tc, mono)
+
+    if tc == 1:
+        # Advection routine
+        adv_2d(simulation, True)
+    elif tc == 2:
+        # Advection error analysis
+        error_analysis_adv2d(simulation)
+    else:
+        print('Invalid testcase!\n')
+        exit()
+
+elif eq == 2:
+    print('Not implemented yet.')
+
 else:
     print('Invalid testcase!\n')
     exit()
