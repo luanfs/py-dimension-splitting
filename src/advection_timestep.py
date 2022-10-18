5aadda0f8413eba0880a606b8daf2c50d8826d8c
luanfs <luanfonseca93@gmail.com>
2022-10-18 10:04:30 -0300

    initial commit

---
+++ src/advection_timestep.py
@@ -0,0 +1,63 @@
+####################################################################################
+# This module contains the routine that computes one advection timestep
+# Luan da Fonseca Santos - October 2022
+####################################################################################
+
+import numpy as np
+from dimension_splitting import F_operator, G_operator
+from advection_ic        import velocity_adv_2d
+from stencil             import flux_ppm_x_stencil_coefficients, flux_ppm_y_stencil_coefficients
+from cfl                 import cfl_x, cfl_y
+
+def adv_timestep(Q, u_edges, v_edges, F, G, FQ, GQ, flux_x, flux_y, ax, cx, cx2, ay, cy, cy2, Xu, Yu, Xv, Yv, t, simulation):
+    N  = simulation.N    # Number of cells in x direction
+    M  = simulation.M    # Number of cells in y direction
+
+    # Ghost cells
+    ngl = simulation.ngl
+    ngr = simulation.ngr
+    ng  = simulation.ng
+
+
+    # Grid interior indexes
+    i0 = simulation.i0
+    iend = simulation.iend
+    j0 = simulation.j0
+    jend = simulation.jend
+
+
+    # Applies F and G operators
+    F_operator(FQ, Q, u_edges, flux_x, ax, cx, cx2, simulation)
+    G_operator(GQ, Q, v_edges, flux_y, ay, cy, cy2, simulation)
+
+    # Applies F and G operators again
+    F_operator(F, Q + 0.5*GQ, u_edges, flux_x, ax, cx, cx2, simulation)
+    G_operator(G, Q + 0.5*FQ, v_edges, flux_y, ay, cy, cy2, simulation)
+
+    # Update
+    Q[i0:iend,j0:jend] = Q[i0:iend,j0:jend] + F[i0:iend,j0:jend] + G[i0:iend,j0:jend]
+
+    # Periodic boundary conditions
+    # x direction
+    Q[iend:N+ng,:] = Q[i0:i0+ngr,:]
+    Q[0:i0,:]      = Q[N:N+ngl,:]
+
+    # y direction
+    Q[:,jend:N+ng] = Q[:,j0:j0+ngr]
+    Q[:,0:j0]      = Q[:,M:M+ngl]
+
+    # Velocity and  CFL numbers update - only for time dependent velocity
+    if simulation.vf == 2:
+        # Velocity update
+        u_edges[:,:], _ = velocity_adv_2d(Xu, Yu, t, simulation)
+        _, v_edges[:,:] = velocity_adv_2d(Xv, Yv, t, simulation)
+
+        # CFL at edges - x direction
+        cx, cx2 = cfl_x(u_edges, simulation)
+
+        # CFL at edges - y direction
+        cy, cy2 = cfl_y(v_edges, simulation)
+
+        # Coefs update
+        flux_ppm_x_stencil_coefficients(u_edges, ax, cx, cx2, simulation)
+        flux_ppm_y_stencil_coefficients(v_edges, ay, cy, cy2, simulation)
---
+++ src/diagnostics.py
@@ -0,0 +1,117 @@
+####################################################################################
+#
+# Module for diagnostic computation and output routines
+#
+# Luan da Fonseca Santos - October 2022
+# (luan.santos@usp.br)
+####################################################################################
+
+import numpy as np
+from errors import compute_errors
+from advection_ic  import qexact_adv_2d, velocity_adv_2d
+from parameters_2d import graphdir
+from plot import plot_2dfield_graphs
+
+####################################################################################
+# Diagnostic variables computation
+####################################################################################
+def diagnostics_adv_2d(Q_average, simulation, total_mass0):
+    N  = simulation.N    # Number of cells in x direction
+    M  = simulation.M    # Number of cells in y direction
+    dx = simulation.dx   # Grid spacing in x direction
+    dy = simulation.dy   # Grid spacing in y direction
+
+    # Grid interior indexes
+    i0 = simulation.i0
+    iend = simulation.iend
+    j0 = simulation.j0
+    jend = simulation.jend
+
+    total_mass =  np.sum(Q_average[i0:iend,j0:jend])*dx*dy  # Compute new mass
+    if abs(total_mass0)>10**(-10):
+        mass_change = abs(total_mass0-total_mass)/abs(total_mass0)
+    else:
+        mass_change = abs(total_mass0-total_mass)
+    return total_mass, mass_change
+
+####################################################################################
+# Print the diagnostics variables on the screen
+####################################################################################
+def print_diagnostics_adv_2d(error_linf, error_l1, error_l2, mass_change, t, Nsteps):
+    print('\nStep', t, 'from', Nsteps)
+    print('Error (Linf, L1, L2) :',"{:.2e}".format(error_linf), "{:.2e}".format(error_l1), "{:.2e}".format(error_l2))
+    print('Total mass variation:', "{:.2e}".format(mass_change))
+
+####################################################################################
+# Output and plot
+####################################################################################
+def output_adv(Xc, Yc, simulation, Q, error_linf, error_l1, error_l2, plot, k, t, Nsteps, plotstep, total_mass0, CFL):
+    N  = simulation.N    # Number of cells in x direction
+    M  = simulation.M    # Number of cells in y direction
+    dx = simulation.dx
+    dt = simulation.dt
+
+    # Grid interior indexes
+    i0 = simulation.i0
+    iend = simulation.iend
+    j0 = simulation.j0
+    jend = simulation.jend
+
+    if plot or k==Nsteps:
+        # Compute exact solution
+        q_exact = qexact_adv_2d(Xc, Yc, t, simulation)
+
+        # Diagnostic computation
+        total_mass, mass_change = diagnostics_adv_2d(Q, simulation, total_mass0)
+
+        # Relative errors in different metrics
+        error_linf[k], error_l1[k], error_l2[k] = compute_errors(Q[i0:iend,j0:jend], q_exact)
+
+        if error_linf[k] > 10**(2):
+            print('\nStopping due to large errors.')
+            exit()
+
+        # Diagnostic computation
+        total_mass, mass_change = diagnostics_adv_2d(Q, simulation, total_mass0)
+
+        if plot and k>0:
+            # Print diagnostics on the screen
+            print_diagnostics_adv_2d(error_linf[k], error_l1[k], error_l2[k], mass_change, k, Nsteps)
+
+        if k%plotstep==0 or k==0 or k==Nsteps :
+            # Plot the graph and print diagnostic
+            # simulation parameters
+            x0 = simulation.x0
+            xf = simulation.xf
+            y0 = simulation.y0
+            yf = simulation.yf
+            ic = simulation.ic
+            tc = simulation.tc
+            icname = simulation.icname
+
+            # Plotting variables
+            nplot = 20
+            xplot = np.linspace(x0, xf, nplot)
+            yplot = np.linspace(y0, yf, nplot)
+            xplot, yplot = np.meshgrid(xplot, yplot)
+            Uplot = np.zeros((nplot, nplot))
+            Vplot = np.zeros((nplot, nplot))
+
+            qmin = str(np.amin(Q))
+            qmax = str(np.amax(Q))
+
+            # colorbar range
+            if simulation.ic == 1:
+                Qmin = -0.1
+                Qmax =  2.1
+            elif simulation.ic == 2 or simulation.ic == 4:
+                Qmin = -0.2
+                Qmax =  1.0
+            elif simulation.ic == 3:
+                Qmin = -0.3
+                Qmax =  1.3
+            # Plot
+            Uplot[0:nplot,0:nplot], Vplot[0:nplot,0:nplot]  = velocity_adv_2d(xplot, yplot, t, simulation)
+            filename = graphdir+'2d_adv_tc'+str(tc)+'_ic'+str(ic)+'_t'+str(k)+'_N'+str(N)+'_'+simulation.fvmethod+'_mono'+simulation.monot+'.png'
+            title = '2D advection - '+icname+' - time='+str(t)+', CFL='+str(CFL)+',\n N='+str(N)+', '+simulation.fvmethod+', mono = '+simulation.monot+ ', Min = '+ qmin +', Max = '+qmax
+            plot_2dfield_graphs([Q[i0:iend,j0:jend]], [Qmin], [Qmax],Xc, Yc, [Uplot], [Vplot], xplot, yplot, filename, title)
